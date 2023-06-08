6e575c23e0d7c4e2fbfbcbe52a162352451f3097
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-06-08 12:13:04 -0500

    feat: :sparkles: realiza calculos sin eval

--- frankenstein.php
+++ frankenstein.php
@@ -35,20 +35,68 @@
     <h2>Calculadora</h2>
     <div class="calculator">
       <div class="result">
-        <?php
-          session_start();
-          if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (isset($_POST['number'])) {
-              $_SESSION['expression'] .= $_POST['number'];
-            } elseif (isset($_POST['operation'])) {
-              $_SESSION['expression'] .= $_POST['operation'];
-            } elseif (isset($_POST['calculate'])) {
-              eval('$result = ' . $_SESSION['expression'] . ';');
-              $_SESSION['expression'] = $result;
-            }
-          }
-          echo $_SESSION['expression'];
-        ?>
+      <?php
+session_start();
+
+function calculateExpression($expression) {
+  // Elimina espacios en blanco innecesarios
+  $expression = trim($expression);
+
+
+  // Divide la expresión en tokens numéricos y operadores
+  $tokens = preg_split('/([\+\-\*\/])/', $expression, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+
+  // Realiza las operaciones en orden de precedencia
+  $operators = ['*', '/', '+', '-'];
+
+  foreach ($operators as $operator) {
+    while (in_array($operator, $tokens, true)) {
+      $operatorIndex = array_search($operator, $tokens, true);
+      $operand1 = $tokens[$operatorIndex - 1];
+      $operand2 = $tokens[$operatorIndex + 1];
+      $result = performOperation($operand1, $operator, $operand2);
+
+      // Reemplaza los operandos y el operador por el resultado en los tokens
+      array_splice($tokens, $operatorIndex - 1, 3, $result);
+    }
+  }
+
+  // Devuelve el resultado final
+  return $tokens[0];
+}
+
+function performOperation($operand1, $operator, $operand2) {
+  switch ($operator) {
+    case '+':
+      return $operand1 + $operand2;
+    case '-':
+      return $operand1 - $operand2;
+    case '*':
+      return $operand1 * $operand2;
+    case '/':
+      return $operand1 / $operand2;
+    default:
+      return null;
+  }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (isset($_POST['number'])) {
+    $_SESSION['expression'] .= $_POST['number'];
+  } elseif (isset($_POST['operation'])) {
+    $_SESSION['expression'] .= $_POST['operation'];
+  } elseif (isset($_POST['calculate'])) {
+    $result = calculateExpression($_SESSION['expression']);
+    $_SESSION['expression'] = $result;
+
+  }
+}
+
+echo $_SESSION['expression'];
+?>
+
+
+
       </div>
       <form method="POST">
         <div class="buttons">
